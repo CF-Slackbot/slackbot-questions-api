f2c8e13c7259d8fe2753ce9e2615300781ba00ad
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-21 14:21:17 -0700

    change to schema

--- src/models/question-schema.js
+++ src/models/question-schema.js
@@ -4,23 +4,23 @@ const mongoose = require('mongoose');
 
 const questionSchema = mongoose.Schema({
   question: String,
-  answers: [{
+  answers: {
     answer_a: String,
     answer_b: String,
     answer_c: String,
     answer_d: String,
     answer_e: String,
     answer_f: String
-  }],
+  },
   multiple_correct_answers: String,
-  correct_answers: [{
+  correct_answers: {
     answer_a_correct: String,
     answer_b_correct: String,
     answer_c_correct: String,
     answer_d_correct: String,
     answer_e_correct: String,
     answer_f_correct: String
-  }],
+  },
   correct_answer: String, //"answer_d",
   tags: [
     {
