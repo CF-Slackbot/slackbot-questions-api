646d414da2ff968c0727edb93d40ce13ca867d5d
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-21 15:53:28 -0700

    added acl stuff to user schema

--- src/models/user-schema.js
+++ src/models/user-schema.js
@@ -11,6 +11,48 @@ const userSchema = new mongoose.Schema({
   
 });
 
+users.virtual('token').get(function () {
+  let tokenObject = {
+    username: this.username,
+  }
+  return jwt.sign(tokenObject, process.env.SECRET)
+});
+
+users.virtual('capabilities').get(function () {
+  let acl = {
+    ta: ['read'],
+    faculty: ['read', 'create', 'update'],
+    admin: ['read', 'create', 'update', 'delete']
+  };
+  return acl[this.role];
+});
+
+users.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+// BASIC AUTH
+users.statics.authenticateBasic = async function (username, password) {
+  const user = await this.findOne({ username })
+  const valid = await bcrypt.compare(password, user.password)
+  if (valid) { return user; }
+  throw new Error('Invalid User');
+}
+
+// BEARER AUTH
+users.statics.authenticateWithToken = async function (token) {
+  try {
+    const parsedToken = jwt.verify(token, process.env.SECRET);
+    const user = this.findOne({ username: parsedToken.username })
+    if (user) { return user; }
+    throw new Error("User Not Found");
+  } catch (e) {
+    throw new Error(e.message)
+  }
+}
+
 const userModel = mongoose.model('user', userSchema);
 
 module.exports = userModel;
