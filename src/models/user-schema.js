4685fb424d5c8ce73a4a5f1d6eeba1fbed054ea7
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-22 09:49:50 -0700

    change to user schema

--- src/models/user-schema.js
+++ src/models/user-schema.js
@@ -1,57 +1,58 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, default:'ta', enum: ['admin', 'faculty', 'ta'] } 
+  role: { type: String, required: true, default:'ta', enum: ['admin', 'faculty', 'ta'] },
+  connection: { type: String, default: 'CF-Slackbot-DB' }
   
 });
 
-userSchema.virtual('token').get(function () {
-  let tokenObject = {
-    username: this.username,
-  }
-  return jwt.sign(tokenObject, process.env.SECRET)
-});
+// userSchema.virtual('token').get(function () {
+//   let tokenObject = {
+//     username: this.username,
+//   }
+//   return jwt.sign(tokenObject, process.env.SECRET)
+// });
 
-userSchema.virtual('capabilities').get(function () {
-  let acl = {
-    ta: ['read'],
-    faculty: ['read', 'create', 'update'],
-    admin: ['read', 'create', 'update', 'delete']
-  };
-  return acl[this.role];
-});
+// userSchema.virtual('capabilities').get(function () {
+//   let acl = {
+//     ta: ['read'],
+//     faculty: ['read', 'create', 'update'],
+//     admin: ['read', 'create', 'update', 'delete']
+//   };
+//   return acl[this.role];
+// });
 
-userSchema.pre('save', async function () {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-});
+// userSchema.pre('save', async function () {
+//   if (this.isModified('password')) {
+//     this.password = await bcrypt.hash(this.password, 10);
+//   }
+// });
 
-// BASIC AUTH
-userSchema.statics.authenticateBasic = async function (username, password) {
-  const user = await this.findOne({ username })
-  const valid = await bcrypt.compare(password, user.password)
-  if (valid) { return user; }
-  throw new Error('Invalid User');
-}
+// // BASIC AUTH
+// userSchema.statics.authenticateBasic = async function (username, password) {
+//   const user = await this.findOne({ username })
+//   const valid = await bcrypt.compare(password, user.password)
+//   if (valid) { return user; }
+//   throw new Error('Invalid User');
+// }
 
-// BEARER AUTH
-userSchema.statics.authenticateWithToken = async function (token) {
-  try {
-    const parsedToken = jwt.verify(token, process.env.SECRET);
-    const user = this.findOne({ username: parsedToken.username })
-    if (user) { return user; }
-    throw new Error("User Not Found");
-  } catch (e) {
-    throw new Error(e.message)
-  }
-}
+// // BEARER AUTH
+// userSchema.statics.authenticateWithToken = async function (token) {
+//   try {
+//     const parsedToken = jwt.verify(token, process.env.SECRET);
+//     const user = this.findOne({ username: parsedToken.username })
+//     if (user) { return user; }
+//     throw new Error("User Not Found");
+//   } catch (e) {
+//     throw new Error(e.message)
+//   }
+// }
 
 const userModel = mongoose.model('user', userSchema);
 
