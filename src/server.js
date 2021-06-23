622225505bb56872e394aaf835107ce66d6f3b45
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-06-23 10:33:46 -0700

    restore problems route for querying

--- src/server.js
+++ src/server.js
@@ -1,39 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const notFound = require('./error-handlers/404.js');
-const errorHandler = require('./error-handlers/500.js');
+const notFound = require("./error-handlers/404.js");
+const errorHandler = require("./error-handlers/500.js");
 
-const apiRoutes = require('../src/routes/api-routes.js');
-const userRoutes = require('../src/routes/user-routes.js');
-const resultsRoutes = require('../src/routes/results-routes.js');
+const apiRoutes = require("../src/routes/api-routes.js");
 
-const v2Routes = require('./routes/v2.js');
+const v2Routes = require("./routes/v2.js");
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(apiRoutes);
-app.use(userRoutes);
-app.use(resultsRoutes);
 
-app.use('/api/v2',v2Routes);
+app.use("/api/v2", v2Routes);
 
-app.use('*', notFound);
+app.use("*", notFound);
 app.use(errorHandler);
 
 module.exports = {
   server: app,
-  start: port => {
+  start: (port) => {
     app.listen(port, () => {
       console.log(`Gurrl we up on: ${port}`);
     });
-  }
+  },
 };
